0199bab6cf99565b57a889f4a3e2df6b6ddeefb2
sonepanpan <sonepanpan@gmail.com>
2021-09-02 15:37:00 +0800

    Initial Commit

---
+++ MetroMeasure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MetroMeasure
+//
+//  Created by 潘婷蓁 on 2021/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MetroMeasure/MetroMeasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroMeasureApp.swift
+//  MetroMeasure
+//
+//  Created by 潘婷蓁 on 2021/9/2.
+//
+
+import SwiftUI
+
+@main
+struct MetroMeasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
